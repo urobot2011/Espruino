7fc0d0e9695e7e6a992a552d28e4b035290a4235
jeffmer <jeffreymagee@gmail.com>
2022-02-25 15:50:45 +0000

    ROCK update

--- boards/ROCK_OLD.py
+++ boards/ROCK_OLD.py
@@ -10,7 +10,7 @@ info = {
  'link' :  [ "https://www.kospet.com/products/kospet-magic-3" ],
  'espruino_page_link' : 'ROCK',
  'default_console' : "EV_BLUETOOTH",
- 'variables' : 14000-9000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 14000-300, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
  'binary_name' : 'espruino_%v_rock.hex',
  'build' : {
@@ -26,8 +26,6 @@ info = {
 #     'DEFINES += -DCONFIG_GPIO_AS_PINRESET', # Allow the reset pin to work
 #     'CFLAGS += -D__STARTUP_CLEAR_BSS -D__START=main',
 #     'LDFLAGS += -D__STARTUP_CLEAR_BSS -D__START=main -nostartfiles',
-     'DEFINES += -DCONFIG_NFCT_PINS_AS_GPIOS', 
-     'DEFINES += -DESPR_LSE_ENABLE', # Ensure low speed external osc enabled
      'DEFINES += -DNRF_SDH_BLE_GATT_MAX_MTU_SIZE=131', #59 77 131 104
      'DEFINES+=-DUSE_FONT_6X8 -DGRAPHICS_PALETTED_IMAGES -DGRAPHICS_ANTIALIAS',
      'DEFINES += -DBLUETOOTH_NAME_PREFIX=\'"ROCK"\'',
@@ -36,13 +34,13 @@ info = {
      'DFU_PRIVATE_KEY=targets/nrf5x_dfu/dfu_private_key.pem',
      'DFU_SETTINGS=--application-version 0xff --hw-version 52 --sd-req 0xa9,0xae,0xb6', #S140 6.0.0
      'BOOTLOADER_SETTINGS_FAMILY=NRF52840',
-     'USE_LCD_SPI_BUF=1',
-     'DEFINES+=-DESPR_GRAPHICS_INTERNAL=1',
+     'USE_LCD_SPI_UNBUF=1',
+     'DEFINES+= -DSPISENDMANY_BUFFER_SIZE=120 -DLCD_SPI_DOUBLEBUFF=1',
      'DEFINES += -DESPR_USE_SPI3 -DSPI0_USE_EASY_DMA=1',
      'ESPR_BLUETOOTH_ANCS=1', # Enable ANCS (Apple notifications) support
 #     'BLACKLIST=boards/MAGIC3.blocklist', # force some stuff to be removed to save space
      'DEFINES += -DNRF_BL_DFU_INSECURE=1 -DNRF_BOOTLOADER_NO_WRITE_PROTECT=1  -DSPIFLASH_SLEEP_CMD=1  -DESPR_DCDC_ENABLE=1',
-      'DEFINES += -DNO_DUMP_HARDWARE_INITIALISATION -DUSE_FONT_6X8',
+     'DEFINES += -DNO_DUMP_HARDWARE_INITIALISATION -DUSE_FONT_6X8',
 #     'DEFINES += -DSAVE_ON_FLASH_SAVE -DSAVE_ON_FLASH_ERRORMSG -DSAVE_ON_FLASH_RANDOM -DSAVE_ON_FLASH_WAVEFORM -DSAVE_ON_FLASH_MATH -DSAVE_ON_FLASH_SWSERIAL -DSAVE_ON_FLASH_FFT -DSAVE_ON_FLASH_DUMP',
 #     'DEFINES+=-DDUMP_IGNORE_VARIABLES=\'"g\\0"\'',
      'DEFINES += -DFDS_VIRTUAL_PAGES=10', #should match fstorage_pages below
@@ -70,12 +68,12 @@ chip = {
   'adc' : 1,
   'dac' : 0,
   'saved_code' : {
-  #'address' : ((0xf8 - fstorage_pages - save_code_pages) * 4096), # Bootloader at 0xF8000
+  'address' : ((0xf8 - fstorage_pages - save_code_pages) * 4096), # Bootloader at 0xF8000
   'page_size' : 4096,
-  #'pages' : save_code_pages,
+  'pages' : save_code_pages,
   'flash_available' : 1024 - ((0x26 + (0x100-0xf8) + fstorage_pages + save_code_pages)*4), # Softdevice uses 38 pages of flash (0x26000/0x100), bootloader 0x100-0xe0=0x20, FS 2, code 96. Each page is 4 kb.
-  'address' : 0x60000000, # put this in external spiflash (see below) - temp reduce size as toxic s/w at start
-  'pages' : 2048, # Entire 8MB of external flash
+  'address2' : 0x60000000, # put this in external spiflash (see below) - temp reduce size as toxic s/w at start
+  'pages2' : 2048, # Entire 8MB of external flash
    },
 };
 
@@ -93,19 +91,6 @@ devices = {
             'pin_rst' : 'D23', # no reset but this is HOLD pin, we want it set to 1 like RST
             'size' : 8192*1024, # 4MB
             'memmap_base' : 0x60000000,
-          },
-
-
-  'LCD' : {
-            'width' : 240, 'height' : 280, 'bpp' : 16, 
-            'controller' : 'st7789',
-            'pin_dc' : 'D47',
-            'pin_cs' : 'D3',
-            'pin_rst' : 'D2',
-            'pin_sck' : 'D45',
-            'pin_mosi' : 'D44',
-            'pin_bl' : 'D12', 
-            'bitrate' : 32000000
           }
 
 
